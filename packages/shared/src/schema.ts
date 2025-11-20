97890323a485972b998dfbf6c00cfcc728fa53a6
ipon1207 <ipon_1207@yahoo.co.jp>
2025-11-20 06:28:44 +0000

    feat: purchaseSchemaおよびsetPurchaseLogSchemaを定義し、型推論を更新

--- packages/shared/src/schema.ts
+++ packages/shared/src/schema.ts
@@ -1,7 +1,15 @@
 import z from 'zod';
 
-export const janCodeSchema = z.object({
+// /api/purchaseに送るオブジェクトのスキーマ定義
+export const purchaseSchema = z.object({
     janCode: z.string(),
 });
 
-export type JanCode = z.infer<typeof janCodeSchema>;
+// /api/setPurchaseLogに送るオブジェクトのスキーマ定義
+export const setPurchaseLogSchema = z.object({
+    productId: z.number(),
+    price: z.number(),
+});
+
+export type JanCode = z.infer<typeof purchaseSchema>;
+export type SetPurchaseLog = z.infer<typeof setPurchaseLogSchema>;
