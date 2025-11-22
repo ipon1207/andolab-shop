9dd7693a0d663c6ef2f99cc95fd1300d72f89dae
ipon1207 <ipon_1207@yahoo.co.jp>
2025-11-22 14:07:07 +0900

    refactor: 購入処理APIをモジュール化し、スキーマ定義を整理

--- packages/shared/src/schema.ts
+++ packages/shared/src/schema.ts
@@ -1,9 +1,15 @@
 import z from 'zod';
 
-// 購入履歴処理の引数のスキーマ定義
-export const LogData = z.object({
+// 購入処理のリクエストボディの型定義
+export const purchaseSchema = z.object({
+    janCode: z.string(),
+});
+
+// 購入履歴記録の引数の型定義
+export const logSchema = z.object({
     productId: z.number(),
     price: z.number(),
 });
 
-export type LogData = z.infer<typeof LogData>;
+export type PurchaseData = z.infer<typeof purchaseSchema>;
+export type LogData = z.infer<typeof logSchema>;
