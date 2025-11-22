091d8d109946923f832127bca9925182431c47df
ipon1207 <ipon_1207@yahoo.co.jp>
2025-11-22 13:46:58 +0900

    refactor: 購入処理リポジトリを新規作成し、商品検索、在庫減少、購入履歴記録機能を追加

--- packages/shared/src/schema.ts
+++ packages/shared/src/schema.ts
@@ -1,15 +1,9 @@
 import z from 'zod';
 
-// /api/purchaseに送るオブジェクトのスキーマ定義
-export const purchaseSchema = z.object({
-    janCode: z.string(),
-});
-
-// /api/setPurchaseLogに送るオブジェクトのスキーマ定義
-export const setPurchaseLogSchema = z.object({
+// 購入履歴処理の引数のスキーマ定義
+export const LogData = z.object({
     productId: z.number(),
     price: z.number(),
 });
 
-export type JanCode = z.infer<typeof purchaseSchema>;
-export type SetPurchaseLog = z.infer<typeof setPurchaseLogSchema>;
+export type LogData = z.infer<typeof LogData>;
