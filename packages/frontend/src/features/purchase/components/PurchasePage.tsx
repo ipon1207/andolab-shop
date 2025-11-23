c6b2faabc4d645e23eccc6c2c3a131278d5c6053
ipon1207 <ipon_1207@yahoo.co.jp>
2025-11-23 09:20:15 +0900

    refactor: UIの表示メッセージを改善

--- packages/frontend/src/features/purchase/components/PurchasePage.tsx
+++ packages/frontend/src/features/purchase/components/PurchasePage.tsx
@@ -15,17 +15,16 @@ function PurchasePage() {
             <h1 className="tex-2xl font-bold mb-4 mt-4">
                 商品のバーコードを読み取ってください
             </h1>
-            {/* ToDo: バーコードで読み取った商品を表示 */}
             <div className="flex flex-row mb-4">
                 <div className="mr-4">
-                    読み取った商品の名前: <span>{lastProduct?.name}</span>
+                    名前: <span>{lastProduct?.name}</span>
                 </div>
                 <div className="ml-4">
-                    読み取った商品の価格: <span>{lastProduct?.price}</span>
+                    価格: <span>{lastProduct?.price}</span>
                 </div>
             </div>
             <div>
-                読み取った商品の在庫数: <span>{lastProduct?.stock}</span>
+                残り在庫数: <span>{lastProduct?.stock}</span>
             </div>
             <button className="mt-4 border rounded-sm px-4 py-2 bg-red-500 text-white hover:bg-red-600">
                 {/* ToDo: 直前の購入を取り消す機能を実装 */}
